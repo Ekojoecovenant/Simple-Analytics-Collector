29151b6397b4a26cc22ed71baca337856b5480f4
Ekojoecovenant <covenantekojoe@gmail.com>
2025-09-27 20:05:09 +0100

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3001;
 // Start server
 const start = async () => {
   try {
-    await fastify.listen({ port: PORT });
+    await fastify.listen({ port: PORT, host: "0.0.0.0" });
     console.log(`Server listening at ${PORT}`);
   } catch (err) {
     fastify.log.error(err);
